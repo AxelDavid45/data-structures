269ec59f77e0593accf5bd30d82643ac259c3a70
AxelDavid45 <axeldavid45@gmail.com>
2019-07-27 15:26:48 -0500

    Repositorio iniciado para estructura de datos

---
+++ src/main.java
@@ -0,0 +1,12 @@
+import java.util.Vector;
+
+public class main {
+    public static void main(String[] args) {
+
+        //Ejemplo de vector
+        Vector vector = new Vector(); //Inicializado en 10 por default
+
+
+
+    }
+}
